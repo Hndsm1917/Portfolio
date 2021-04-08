cad2223fec8c647822959d05a1be25a8ab6a932a
Hndsm1917 <45174726+Hndsm1917@users.noreply.github.com>
2021-04-08 16:39:43 +0300

    Add files via upload

---
+++ home_first_slider.php
@@ -0,0 +1,25 @@
+<section class="home_first_slider">
+    <div class="container">
+        <div class="home_first_slider__grid">
+            <h1>We’re cool kids.</h1>
+            <p>Cool Kids Blogs - international content company the prime aim of which is providing the best content in various forms for people all over the world! Find out more by reading text below.</p>
+            <div class="home_first_slider__more">
+                <div class="home_first_slider__more-block">
+                    <img src="<?= get_template_directory_uri()?>/assets/img/procesed/first_slider_img.png" alt="Contact with Cool kids">
+                    <a href="https://coolkidsblogs.com/partnership/">Become our partner</a>
+                    <a href="#home_contact_us__input">CONTACT US</a>
+                    <a href="#home_express">Career</a>
+                </div>
+                
+            </div>
+        </div>
+        <div class="home_first_slider__arrow">
+            <p>Scroll</p>
+            <svg width="8" height="53" viewBox="0 0 8 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.64645 52.3536C3.84171 52.5488 4.15829 52.5488 4.35356 52.3536L7.53554 49.1716C7.7308 48.9763 7.7308 48.6597 7.53554 48.4645C7.34027 48.2692 7.02369 48.2692 6.82843 48.4645L4 51.2929L1.17158 48.4645C0.976313 48.2692 0.65973 48.2692 0.464468 48.4645C0.269206 48.6597 0.269206 48.9763 0.464468 49.1716L3.64645 52.3536ZM3.5 2.18557e-08L3.5 52L4.5 52L4.5 -2.18557e-08L3.5 2.18557e-08Z" fill="white"/>
+            </svg>
+        </div>
+    </div>
+    <canvas id="home_first_slider__scene"></canvas>
+    
+</section>
